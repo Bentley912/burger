b22a6e388cdc6c0b78396bffe6c4944e8dcd846c
Patrick <pbentley07@gmail.com>
2017-05-07 23:04:42 -0400

    Started ORM Construction - All Completed

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,20 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL){
+    connection=mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+ mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
   password: "root",
   database: "burgers_db"
 });
+}
+
+connection.connection();
+  
 // Make connection.
 connection.connect(function(err) {
   if (err) {
--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,17 @@
-var connection = require('./connection.js');
+var connection = require('./connection');
+
+
+var orm = {
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+        if (err) {
+            throw err;
+        }
+        cb(result);
+        });
+    },
+    
+}
+
+module.exports = orm;
